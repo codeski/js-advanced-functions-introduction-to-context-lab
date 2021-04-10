57b777f5d902f301884dbe7eab5988f5d128608a
Todd Stoneberg <todd.stoneberg@gmail.com>
2021-04-10 14:51:51 -0600

    Done.

--- index.js
+++ index.js
@@ -20,14 +20,14 @@ function createTimeOutEvent(obj, dateTime){
 }
 
 function hoursWorkedOnDate(obj, date){
-    let timeIn = obj.timeInEvents.find(function(obj){return obj.date = date})
-    let timeOut = obj.timeOutEvents.find(function(obj){return obj.date = date})
+    let timeIn = obj.timeInEvents.find(function(obj){return obj.date === date})
+    let timeOut = obj.timeOutEvents.find(function(obj){return obj.date === date})
     timeIn = timeIn.hour
     timeOut = timeOut.hour
     let hourIn = parseInt((""+timeIn).slice(0, -2))
     let hourOut = parseInt((""+timeOut).slice(0, -2))
-    // console.log(hourOut)
     return hourOut - hourIn
+    
 }
 
 function wagesEarnedOnDate(obj, date){
@@ -36,9 +36,32 @@ function wagesEarnedOnDate(obj, date){
 }
 
 function allWagesFor(obj){
-    let allTimeInEventDates = obj.timeInEvents.map(timeInObj => timeInObj.date)
-    console.log("Time in dates", allTimeInEventDates)
-    let allWagesArray = allTimeInEventDates.map(date => wagesEarnedOnDate(obj, date))
-    console.log(allWagesArray)
-    
+    let allTimeInDates = obj.timeInEvents.map(timeInObj => timeInObj.date)
+    let wagesArray = allTimeInDates.map(date => wagesEarnedOnDate(obj, date))
+    let totalWages = wagesArray.reduce((total, currentValue) => total + currentValue)
+    return totalWages
+    // let pay = obj.payPerHour
+    // let allTimeInHours = obj.timeInEvents.map(timeInObj => timeInObj.hour)
+    // let allTimeOutHours = obj.timeOutEvents.map(timeOutObj => timeOutObj.hour)
+    // let totalHoursArray = []
+    // for (let i = 0; allTimeInHours.length > i; i++) {
+    //     totalHoursArray.push(allTimeOutHours[i] - allTimeInHours[i])
+    // }
+    // let x = totalHoursArray.map(i => i.toString())
+    // x = x.map(i => i.replace(/(0){2}$/i, ''))
+    // x = x.map(i => parseInt(i))
+    // let totalHours = x.reduce((total, currentValue) => total + currentValue)
+    // return totalHours * pay
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+    let obj = srcArray.find(obj => obj.firstName === firstName)
+    return obj
+}
+
+function calculatePayroll(arrayOfEmployees){
+    console.log(arrayOfEmployees)
+    let totalWagesArray = arrayOfEmployees.map(obj => allWagesFor(obj))
+    let grandTotal = totalWagesArray.reduce((total, currentValue) => total + currentValue)
+    return grandTotal
 }
